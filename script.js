660b2a5d40772ba3c57e4f43b917bd3acd923985
JopJenn <86079239+JopJenn@users.noreply.github.com>
2021-07-12 18:29:39 -0400

    Add files via upload
    Week 3 lab 1

---
+++ script.js
@@ -0,0 +1,7 @@
+let actualTemp = "warm";
+let desiredTemp = "cool";
+if (desiredTemp === "cool" && !Standby) {
+ console.log(" is not tired");
+} else {
+ console.log("Something is not right...");
+}
