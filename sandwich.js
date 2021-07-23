71fafc407a1fbb2c8496e17eff25dd797aa4989a
JopJenn <86079239+JopJenn@users.noreply.github.com>
2021-07-22 20:35:59 -0400

    Add files via upload
    assessment 2 - Jennifer Joplin

---
+++ sandwich.js
@@ -0,0 +1,45 @@
+//#1
+function countDown(){
+    for (let i=5; i >= 1; i--){
+        console.log(i)
+    }
+} 
+countDown()
+
+//#2
+function countUp(start, end) {
+    for(let i= start; i < end; i+=2){
+            console.log(i)
+        }
+                
+}
+countUp(0, 20)
+
+
+//#3 
+// function isLong(word) {
+//     if(let i=)
+
+// }
+// //#4
+// function whatWaterState(temperature){
+//     const temperature = x
+//     if (let x)
+// }
+// //#5 var sandwiches, initialized to array
+// var sandwiches =
+// [
+//     {"type": "italian", "calories": 1000, "cost": 7.69, "isVegetarian": false},
+//     {"type": "veggie", "calories": 500, "cost": 5.50, "isVegetarian": true},
+//     {"type": "meatball", "calories": 900, "cost": 8.25, "isVegetarian": false},
+//     {"type": "jackfruit", "calories": 650, "cost": 8.50, "isVegetarian": true},
+// ]
+// console.log(sandwiches);
+
+// //#6declare function names makeSandwich
+// function makeSandwich(sandwichArray, type, calories,cost, isVegetarian)
+// const newObject = {
+//     type: newType, calories: newCalories, cost: NewCost, passed:isNewVegetarian >= Vegetarian ? true : false
+
+//     Array.ret
+// };
